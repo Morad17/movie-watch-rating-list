49835b7acf31cf5b60e951bad09eaa68ba9b4cbc
Morad17 <morad123@hotmail.co.uk>
2023-02-28 17:26:58 +0000

    sorted out env issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Home = () => {
+    const [movieData, getmovieData] = useState([])
+
+    useEffect(()=> {
+        axios.get(process.env.REACT_APP_MOVIES_URL)
+        .then(response => {
+          console.log(response.data)
+        }) 
+        
+    })
+
   return (
-    <div>Home</div>
+    <div>test
+    </div>
   )
 }
 
