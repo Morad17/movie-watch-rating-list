f508e3da6b9f0d09eac8e9d2e5573cea9dc31035
Morad17 <morad123@hotmail.co.uk>
2023-03-02 23:18:31 +0000

    css updates, started backend mongoose

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const MainNavbar = () => {
+  
   return (
-    <div>MainNavbar</div>
-  )
+    <div className="main-navbar">
+      <nav className="top-navbar">
+          <ul>
+            <li className="nav-links">Home</li>
+            <li className="nav-links">Poppularity</li>
+            <li className="nav-links">My Movies</li>
+          </ul>
+      </nav>
+      <nav className="bottom-navbar">
+        <div className="logo">Logo</div>
+        <div className="search-bar">Search bar</div>
+        <div className="login">Members Login</div>
+      </nav>
+    </div>
+    
+    
+    )
 }
 
 export default MainNavbar
